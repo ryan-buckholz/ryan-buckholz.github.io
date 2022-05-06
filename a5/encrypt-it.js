39a2054b7dde6880401087fd242a42a4481ea0b5
Ryan Buckholz <rbuckholz1@gmail.com>
2022-05-05 20:08:08 -0700

    a5

---
+++ a5/encrypt-it.js
@@ -0,0 +1,52 @@
+/*
+ * Starter file 
+ */
+(function() {
+  "use strict";
+
+  /**
+   * The starting point in our program, setting up a listener
+   * for the "load" event on the window, signalling the HTML DOM has been constructed
+   * on the page. When this event occurs, the attached function (init) will be called.
+   */
+  window.addEventListener("load", init);
+  /**
+   * TODO: Write a function comment using JSDoc.
+   */
+  function init() {
+    // Note: In this function, we usually want to set up our event handlers
+    // for UI elements on the page.
+    console.log("Window loaded!");
+    document.getElementById("encrypt-it").addEventListener("click", encryptBtnClicked);
+  }
+
+  function encryptBtnClicked(){
+    var input = document.getElementById("input-text");
+    var inputtext = input.value;
+    var outputText = shiftCipher(inputtext);
+    document.getElementById("result").innerText = outputText;
+  }
+
+  function shiftCipher(text) {
+    text = text.toLowerCase();
+    let result = "";
+    for (let i = 0; i < text.length; i++) {
+      if (text[i] < 'a' || text[i] > 'z') {
+        result += text[i];
+      } else if (text[i] == 'z') {
+        result += 'a';
+      } else { // letter is between 'a' and 'y'
+        let letter = text.charCodeAt(i);
+        let resultLetter = String.fromCharCode(letter + 1);
+        result += resultLetter;
+      }
+    }
+    return result;
+  }
+
+  // Add any other functions in this area (you should not implement your
+  // entire program in the init function, for similar reasons that
+  // you shouldn't write an entire Java program in the main method).
+  
+
+})();
